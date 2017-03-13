cd1c79d2b946d1b2ecec0688c18671701342c679
pgajer <pgajer@gmail.com>
2017-03-13 07:44:35 -0400

    previous version in some cases was wiping out an input file; this one does not seem to do it

--- perl/select_seqs.pl
+++ perl/select_seqs.pl
@@ -1,4 +1,4 @@
-#! /usr/bin/perl
+#!/usr/bin/env perl
 
 =head1 NAME
 
@@ -6,19 +6,22 @@
 
 =head1  DESCRIPTION
 
-  It is a faster version of select_seqs.pl
-
-  select sequences from a single fasta file given a list of sequence IDs
-  selected sequences are place in the new fasta file in the order they are listed
+  Select sequences from a single fasta file given a list of sequence
+  IDs. Selected sequences are place in the new fasta file in the order they are
+  listed.
 
 =head1  INPUT
 
-    file with readIds of sequences to be selected
-    fasta file to select from
+  - file with readIds of sequences to be selected
+  - fasta file to select from
 
 =head1  OUTPUT
 
-    fasta file of selected sequences
+  - fasta file of selected sequences
+
+=head1 SYNOPSIS
+
+    select_seqs.pl [-s|-e] <list of seqIDs file> -i <fastaFile> -o <outFile>
 
 =head1 OPTIONS
 
@@ -29,30 +32,21 @@
     Print help message and exit successfully.
 
 =item B<--fasta-file, -i>
-
-    fasta file
+  Fasta file
 
 =item B<--sels-file, -s>
-
-  a file with a list of sequence IDs to be selected from the input fasta file
+  File with a list of sequence IDs to be selected from the input fasta file
 
 =item B<--excl-file, -e>
-
-  a file with a list of sequence IDs to be excluded from the input fasta file
+  File with a list of sequence IDs to be excluded from the input fasta file
 
 =item B<--output-file, -o>
-
-    Output file.
+  Output file.
 
 =item B<--quiet>
   Do not print progress messages.
 
 =back
-
-=head1 SYNOPSIS
-
-    select_seqs.pl -s <list of readIDs file> -i <fastaFile> -o <outFile>
-
 
 =head1  EXAMPLES
 
@@ -60,7 +54,7 @@
 
 =cut
 
-## Copyright (C) 2015 Pawel Gajer pgajer@gmail.com
+## Copyright (C) 2017 Pawel Gajer pgajer@gmail.com
 ##
 ## Permission to use, copy, modify, and distribute this software and its
 ## documentation with or without modifications and for any purpose and
@@ -81,35 +75,52 @@
 ## OR PERFORMANCE OF THIS SOFTWARE.
 ##
 
+
 use strict;
 use warnings;
+use Pod::Usage;
 use English qw( -no_match_vars );
 use Getopt::Long qw(:config no_ignore_case no_auto_abbrev pass_through);
-use Pod::Usage;
 
 $OUTPUT_AUTOFLUSH = 1;
 
+####################################################################
+##                             OPTIONS
+####################################################################
+
 GetOptions(
-  "h|help"          => \my $help,
-  "fasta-file|i=s"  => \my $inFile,
+  "input-file|i=s"  => \my $inFile,
   "sels-file|s=s"   => \my $selsFile,
   "excl-file|e=s"   => \my $exclFile,
   "output-file|o=s" => \my $outFile,
   "quiet"           => \my $quiet,
+  "help|h!"         => \my $help,
   )
-  || pod2usage();
+  or pod2usage(verbose => 0,exitstatus => 1);
 
 
-if ( $help || !$inFile || !$outFile )
+if ($help)
 {
-  pod2usage( { -exitval => 0, -verbose => 2 } );
+  pod2usage(verbose => 2,exitstatus => 0);
   exit;
 }
 
-if ( !$selsFile && !$exclFile )
+if (!$inFile)
 {
-  print "Either -s or -e option has to be used.\n";
-  pod2usage( { -exitval => 0, -verbose => 2 } );
+  print "\n\nERROR: Missing input file\n\n\n";
+  pod2usage(verbose => 2,exitstatus => 0);
+  exit;
+}
+elsif (!$outFile)
+{
+   print "\n\nERROR: Missing output file\n\n\n";
+   pod2usage(verbose => 2,exitstatus => 0);
+   exit;
+}
+
+if ( ! -e $inFile )
+{
+  print "\n\nERROR: $inFile does not exist\n\n\n";
   exit;
 }
 
@@ -120,7 +131,7 @@ if ( -l $inFile )
 
 if ( $selsFile && -l $selsFile )
 {
-  $selsFile = readlink($selsFile);
+   $selsFile = readlink($selsFile);
 }
 
 if ( $exclFile && -l $exclFile )
@@ -128,132 +139,53 @@ if ( $exclFile && -l $exclFile )
   $exclFile = readlink($exclFile);
 }
 
-
 ####################################################################
 ##                               MAIN
 ####################################################################
 
-my $startRun = time();
-my $endRun = time();
-my $runTime = $endRun - $startRun;
-my $timeStr;
-my $timeMin = int($runTime / 60);
-my $timeSec = $runTime % 60;
-
-my $fileSize = -s $inFile; # file size in bytes
-
-open (FASTA, "<$inFile") or die "Cannot open $inFile for reading: $OS_ERROR\n";
-open OUT, ">$outFile" or die "Cannot open $outFile for writing: $OS_ERROR\n";
 my $count = 0;
 my $selCount = 0;
 
-if ($selsFile)
+if ( defined $selsFile )
 {
-  print "\r--- Reading seq IDs from $selsFile" if !$quiet;
-
   my %selRead = readArray($selsFile);
-
   my $nSelSeqs = keys %selRead;
 
-  # printKeys(\%selRead,100);
-  # exit;
-  # printTbl(\%selRead); exit;
-  # print "size(selRead): " . keys(%selRead) . "\n"; exit;
-
-  if (!$quiet)
-  {
-    print "\r                                  ";
-  }
-
+  open (FASTA, "<$inFile") or die "Cannot open $inFile for reading: $OS_ERROR\n";
   $/ = ">";
   my $junkFirstOne = <FASTA>;
+
+  open OUT, ">$outFile" or die "Cannot open $outFile for reading: $OS_ERROR\n";
   while (<FASTA>)
   {
-    if ( !$quiet && ($count % 500 == 0) )
-    {
-      $endRun = time();
-      $runTime = $endRun - $startRun;
-      if ( $runTime > 60 )
-      {
-	$timeMin = int($runTime / 60);
-	$timeSec = sprintf("%02d", $runTime % 60);
-	$timeStr = "$timeMin:$timeSec";
-      }
-      else
-      {
-	$runTime = sprintf("%02d", $runTime);
-	$timeStr = "$timeMin:$runTime";
-      }
-
-      my $perc = sprintf("%.1f%%", 100 * (tell FASTA) / $fileSize);
-      print "\r$timeStr [$perc]";
-
-    }
-    $count++;
-
     chomp;
     my ($def,@seqlines) = split /\n/, $_;
     my $seq = join '', @seqlines;
-    my ($id, $size) = split /;|\s+/, $def;
-    ##print "id=$id\tdef=$def\n";exit;
-    ##$size =~ s/size=//;
-    ##print "size=$size\n"; exit;
-
+    my ($id) = split /\s+/, $def;
     if ( exists $selRead{$id} && $selRead{$id}==1 )
     {
-      if ($size)
-      {
-	print OUT ">$def\n$seq\n";
-      }
-      else
-      {
-	print OUT ">$id\n$seq\n";
-      }
+      print OUT ">$id\n$seq\n";
       $selCount++;
       $selRead{$id}++;
-
-      last if $selCount==$nSelSeqs;
     }
-  } ## end of while()
-
-  if ($selCount!=$nSelSeqs)
-  {
-    print "\r                                                           ";
-    warn "\n\tWARNING: Number of sequences selected:       $selCount";
-    warn "  \tWARNING: Number of sequences to be selected: $nSelSeqs";
+    last if $selCount==$nSelSeqs;
+    $count++;
   }
+  close OUT;
+  close FASTA;
+
+  #print "nSelSeqs: $nSelSeqs\n";
 }
 else
 {
   my %exclRead = readArray($exclFile);
 
+  open (FASTA, "<$inFile") or die "Cannot open $inFile for reading: $OS_ERROR\n";
   $/ = ">";
   my $junkFirstOne = <FASTA>;
-
+  open OUT, ">$outFile" or die "Cannot open $outFile for reading: $OS_ERROR\n";
   while (<FASTA>)
   {
-    if (!$quiet && ($count % 500 == 0))
-    {
-      $endRun = time();
-      $runTime = $endRun - $startRun;
-      if ( $runTime > 60 )
-      {
-	$timeMin = int($runTime / 60);
-	$timeSec = sprintf("%02d", $runTime % 60);
-	$timeStr = "$timeMin:$timeSec";
-      }
-      else
-      {
-	$runTime = sprintf("%02d", $runTime);
-	$timeStr = "$timeMin:$runTime";
-      }
-
-      my $perc = sprintf("%.1f%%", 100 * (tell FASTA) / $fileSize);
-      print "\r$timeStr [$perc]";
-
-    }
-    $count++;
-
     chomp;
     my ($def,@seqlines) = split /\n/, $_;
     my $seq = join '', @seqlines;
@@ -263,10 +195,12 @@ else
       print OUT ">$id\n$seq\n";
       $selCount++;
     }
+    $count++;
   }
+  close OUT;
+  close FASTA;
+  $/ = "\n";
 }
-close OUT;
-$/ = "\n";
 
 if (!$quiet)
 {
@@ -275,44 +209,36 @@ if (!$quiet)
   print "  \tNumer of sequences in the output fasta file: $selCount\n\n";
 
   print "\rSelected sequences written to $outFile\n";
-
-  ## report timing
-  $endRun = time();
-  $runTime = $endRun - $startRun;
-  if ( $runTime > 60 )
-  {
-    $timeMin = int($runTime / 60);
-    $timeSec = sprintf("%02d", $runTime % 60);
-    print "\rCompleted in $timeMin:$timeSec\n"
-  }
-  else
-  {
-    print "\rCompleted in $runTime seconds\n"
-  }
 }
-
 
 ####################################################################
 ##                               SUBS
 ####################################################################
 
-# read table with one column
-sub readArray{
+# print array to stdout
+sub printArray
+{
+  my ($a, $header) = @_;
+  print "$header\n" if $header;
+  map {print "$_\n"} @{$a};
+}
 
+# read table with one column
+sub readArray
+{
   my $file = shift;
   my %rows;
 
-  $/ = "\n";
-  open IN, "$file" or die "Cannot open $file for reading: $OS_ERROR\n";
-  foreach (<IN>)
+  #local $/ = '\n';
+  open (INPUT, "<", $file) or die "Cannot open $file for reading: $OS_ERROR\n";
+  foreach (<INPUT>)
   {
     chomp;
     my ($id) = split /\s+/, $_;
     #print "id: $id\n"; exit;
     $rows{$id}=1;
   }
-  close IN;
-#  $/ = ">";
+  close INPUT;
 
   return %rows;
 }
